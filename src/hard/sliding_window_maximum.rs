7703de9c3787144ec0895535fc4906013f8035b0
Xbss <2055761346@qq.com>
2023-09-13 17:29:12 +0800

    max_sliding_window

--- src/hard/sliding_window_maximum.rs
+++ src/hard/sliding_window_maximum.rs
@@ -2,14 +2,128 @@
 #[test]
 pub fn test_max_sliding_window(){
     // println!("{:?}", max_sliding_window(vec![1, 2, 4, 5, 6, 4, 5], 3));
+    // println!("{:?}", max_sliding_window(vec![1], 1));
+    // println!("{:?}", max_sliding_window(vec![1,2,2,1,5], 3));
+    // println!("{:?}", max_sliding_window(vec![1,2,2,1,5], 3));
     // println!("{:?}", max_sliding_window(vec![1,3,-1,-3,5,3,6,7], 3));
-    println!("{:?}", max_sliding_window(vec![1], 1));
+    println!("{:?}", max_sliding_window(vec![1,3,1,2,0,5], 3));
 }
 pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    if nums.len()==1 { return vec![nums[0]] }
+    let k = k as usize;
+    let (mut left,mut right):(usize,usize) = (0,k-1);
+    let mut max_num = compute_max_num2(&nums[0..k]);
+    let mut max_vec = vec![];
+    max_vec.push(max_num.0);
+    while right < nums.len()-1 {
+        right += 1;
+        let i = nums[right];
+        if i>max_num.0 {
+            max_num = (i,1);
+            max_vec.push(i);
+        }else if i==max_num.0 {
+            max_num.1+=1;
+            max_vec.push(i);
+        }else {
+            //此时滑进来的值比窗口内最大值小，需要判断将滑出去的值是不是最大值。
+            if nums[left] == max_num.0 {
+                if max_num.1 >1  {
+                    max_vec.push(max_num.0);
+                    max_num.1 -= 1;
+                }else {
+                    //窗口最大值要滑走了，判断次大值关系
+                    if i>max_num.2 {
+                        max_vec.push(i);
+
+                    }
+                    max_num = compute_max_num2(&nums[left+1..right+1]);
+                    max_vec.push(max_num.0);
+                }
+            }else {
+                max_vec.push(max_num.0);
+            }
+        }
+        left += 1;
+    }
+    max_vec
+}
+pub fn compute_max_num(mut nums: &[i32]) ->(i32, i32, i32, i32){
+    let mut max_num:(i32, i32, i32, i32) = if nums[0]>nums[1]{
+        (nums[0],1,nums[1],1)
+    }else {
+        (nums[1],1,nums[0],1)
+    };
+    nums[2..].iter().for_each(|x|{
+        if *x>max_num.0 {
+            max_num = (*x,1,max_num.0,max_num.1);
+        }else if *x == max_num.0{
+            max_num.1 += 1;
+        }else if *x<max_num.0&&*x>max_num.2 {
+            max_num.2 = *x;
+            max_num.3 = 1;
+        }else if *x<max_num.0&&*x==max_num.2 {
+            max_num.3 += 1;
+        }
+    });
+    max_num
+}
+///右滑一下进入一个值a，左边同步出去一个值b，判断a是否比当前窗口内最大值还大。
+/// 1.b是最大值，但是b滑出去后当前窗口内是否还有b？可以用（最大值，最大值数量）来记录
+/// ①b大于一个，滑出去那么当前窗口最大值就是max（a，b）
+/// ②b只有一个，滑出去了，若a>b还好，直接最大值是a，若a<b呢？当前窗口最大值以及最大值个数需要重新统计.
+/// 时间 984ms 击败 5.66%使用 Rust 的用户
+/// 内存 3.33MB 击败 66.98%使用 Rust 的用户
+pub fn max_sliding_window2(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let k = k as usize;
+    let (mut left,mut right):(usize,usize) = (0,k-1);
+    // let mut max_num = *nums[0..k].iter().max().unwrap();
+    let mut max_num = compute_max_num2(&nums[0..k]);
+    // println!("{:?}",max_num);
+    let mut max_vec = vec![];
+    max_vec.push(max_num.0);
+    while right < nums.len()-1  {
+        right += 1;
+        let i = nums[right];
+        if i>max_num.0 {
+            max_num = (i,1);
+            max_vec.push(i);
+        }else if i==max_num.0 {
+            max_num.1+=1;
+            max_vec.push(i);
+        }else {
+            //此时滑进来的值比窗口内最大值小，需要判断将滑出去的值是不是最大值。
+            if nums[left] == max_num.0 {
+                if max_num.1 >1  {
+                    max_vec.push(max_num.0);
+                    max_num.1 -= 1;
+                }else {
+                    max_num = compute_max_num2(&nums[left+1..right+1]);
+                    max_vec.push(max_num.0);
+                }
+            }else {
+                max_vec.push(max_num.0);
+            }
+        }
+        left += 1;
+    }
+    max_vec
+}
+pub fn compute_max_num2(mut nums: &[i32]) ->(i32, i32){
+    let mut max_num = (nums[0],1);
+    nums[1..].iter().for_each(|x|{
+        if *x>max_num.0 {
+            max_num = (*x,1);
+        }else if *x == max_num.0{
+            max_num.1 += 1;
+        }
+    });
+    max_num
+}
+///超时，每个滑动窗口做一个切片计算最大值
+pub fn max_sliding_window3(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let mut left:usize = 0;
     let k = k as usize;
     let mut max_num = *nums[0..k].iter().max().unwrap();
-
     let mut max_vec = vec![];
     while left + k <= nums.len()  {
         let x = &nums[left..left + k].iter().max().unwrap();

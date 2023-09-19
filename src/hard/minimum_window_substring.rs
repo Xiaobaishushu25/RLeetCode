0e61548db88a266bfd41abc3eef0c3d71cd37c73
Xbss <2055761346@qq.com>
2023-09-19 15:35:34 +0800

    min_window

--- src/hard/minimum_window_substring.rs
+++ src/hard/minimum_window_substring.rs
@@ -20,15 +20,17 @@ mod tests {
     #[test]
     fn test_min_window() {
         // println!("{}", min_window("ADOBECODEBANC".into(), "ABC".into()));
-        println!("{}", min_window("ab".into(), "a".into()));
+        println!("{}", min_window("acbbaca".into(), "aba".into()));
+        // println!("{}", min_window("ab".into(), "a".into()));
     }
     pub fn min_window(s: String, t: String) -> String {
         if t.len()>s.len() { return "".into() }
         // let (mut left,mut right) = (0,0);
         let mut right = 0;
         let mut min_size = s.len()+1;
+        let mut now_sum = 0;
         // let mut flag = s.len()+1;
-        let mut result = String::new();
+        let mut result = String::from("");
         let mut deque = VecDeque::new();
         let mut map = HashMap::with_capacity(t.len());
         for char in t.chars(){
@@ -41,6 +43,7 @@ mod tests {
             deque.push_back(x);
             if map.contains_key(&x){
                 now_map.insert(x,now_map.get(&x).unwrap_or(&0)+1);
+                now_sum+=1;
             }
             loop {
                 if let Some(front) = deque.front(){
@@ -50,6 +53,7 @@ mod tests {
                             // println!("当前{}的数量是{}，需要的数量{}",front,i,need_i);
                             if i-1>=*need_i {
                                 now_map.insert(*front,now_map.get(&front).unwrap()-1);
+                                now_sum-=1;
                                 deque.pop_front();
                             }else {
                                 break
@@ -63,7 +67,7 @@ mod tests {
                     break
                 }
             }
-            if min_size >deque.len() {
+            if min_size >deque.len()&&now_sum>=t.len() {
                 min_size = deque.len();
                 result = deque.iter().collect::<String>();
             }
